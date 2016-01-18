9627b52f84229fbe21f54f983a08e4c0e2f7e3a9
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-18 15:44:02 +0900

    add evolutions file

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+
+CREATE TABLE "user" (
+    "id" bigint(20) NOT NULL AUTO_INCREMENT,
+    "name" varchar(255),
+    "mail" varchar(255),
+    PRIMARY KEY ("id")
+);
+
+# --- !Downs
+
+DROP TABLE "user";
